76aae882de4e52b4ce73d0d9a022a45bd7f2e4c2
meng-xiao <xiaomengphy@gmail.com>
2019-09-11 22:57:46 +0800

    fweb

--- runall.sh
+++ runall.sh
@@ -1,12 +1,17 @@
 for i in {0..4}
 do 
-	j_had=$((-0.2+i*0.1))
-	j_lep=$((-0.1+i*0.0.05))
+	j_had=$(echo "-0.2+$i*0.1" |bc)
+	j_lep=$(echo "-0.1+$i*0.05" |bc)
+	v=$((i+1))
+	echo $j_had $j_lep $v
 #background model preparation
-./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/data_all_had_bdt.root -p bkg -f TTHHadronicTag0,TTHHadronicTag1 --ext hadronic_bdt_tuneV1 --intLumi 41.5 --backgroundOnly --isData --batch none --dataFile /eos/home-x/xiaomeng/ttH/data_all_had_bdt.root --boundaries 0.9675,1 --bdt_boundary 0 & 
+./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/data_all_had_bdt.root -p bkg -f TTHHadronicTag0,TTHHadronicTag1,TTHHadronicTag2,TTHHadronicTag3,TTHHadronicTag4,TTHHadronicTag5 --ext hadronic_bdt_tuneV$v --intLumi 41.5 --backgroundOnly --isData --batch none --dataFile /eos/home-x/xiaomeng/ttH/data_all_had_bdt.root --boundaries 0.9675, 0.9937, 0.9971, 1 --bdt_boundary $j_had & 
+./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/data_all_lep_bdt.root -p bkg -f TTHLeptonicTag0,TTHLeptonicTag1,TTHLeptonicTag2,TTHLeptonicTag3,TTHLeptonicTag4,TTHLeptonicTag5 --ext lepronic_bdt_tuneV$v --intLumi 41.5 --backgroundOnly --isData --batch none --dataFile /eos/home-x/xiaomeng/ttH/data_all_lep_bdt.root --boundaries 0.8435, 0.9346, 0.9625, 1 --bdt_boundary $j_lep & 
 #signal model preparation
-#./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/ttHgg_0M_had_2018_bdt.root -p tth_ALT -f TTHHadronicTag0,TTHHadronicTag1 --ext jhu_0m_tuneV1 --intLumi 41.5 --signalOnly --batch none  
-#./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/ttHgg_0M_had_2018_bdt.root -p tth_ALT -f TTHHadronicTag0,TTHHadronicTag1 --ext jhu_0m_tuneV1 --intLumi 41.5 --signalOnly --batch none --boundaries 0.9675,1 --bdt_boundary 0 
+./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/ttHgg_0M_had_new_2018_bdt.root -p tth_ALT -f TTHHadronicTag0,TTHHadronicTag1,TTHHadronicTag2,TTHHadronicTag3,TTHHadronicTag4,TTHHadronicTag5 --ext hadronic_0m_tuneV$v --intLumi 41.5 --signalOnly --batch none  --boundaries 0.9675, 0.9937, 0.9971, 1 --bdt_boundary $j_had 
+./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/ttHgg_0M_lep_new_2018_bdt.root -p tth_ALT -f TTHHadronicTag0,TTHHadronicTag1,TTHHadronicTag2,TTHHadronicTag3,TTHHadronicTag4,TTHHadronicTag5 --ext lepronic_0m_tuneV$v --intLumi 41.5 --signalOnly --batch none  --boundaries 0.8435, 0.9346, 0.9625, 1 --bdt_boundary $j_lep 
+./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/ttHgg_0PM_had_new_2018_bdt.root -p tth -f TTHHadronicTag0,TTHHadronicTag1,TTHHadronicTag2,TTHHadronicTag3,TTHHadronicTag4,TTHHadronicTag5 --ext hadronic_0p_tuneV$v --intLumi 41.5 --signalOnly --batch none  --boundaries 0.9675, 0.9937, 0.9971, 1 --bdt_boundary $j_had 
+./runFinalFitsScripts.sh -i /eos/home-x/xiaomeng/ttH/ttHgg_0PM_lep_new_2018_bdt.root -p tth -f TTHHadronicTag0,TTHHadronicTag1,TTHHadronicTag2,TTHHadronicTag3,TTHHadronicTag4,TTHHadronicTag5 --ext lepronic_0p_tuneV$v --intLumi 41.5 --signalOnly --batch none  --boundaries 0.8435, 0.9346, 0.9625, 1 --bdt_boundary $j_lep 
 done
 
 
