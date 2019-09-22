b936cc3f5bab9bb676c2d4c0bb0fd9c05035e39c
meng-xiao <xiaomengphy@gmail.com>
2019-09-22 21:57:13 +0800

    fweb

--- Plots/FinalResults/bdt.sh
+++ Plots/FinalResults/bdt.sh
@@ -10,11 +10,11 @@ do
 	ln -s /afs/cern.ch/work/x/xiaomeng/test/myWorkingArea/CMSSW_8_1_0/src/flashggFinalFit/Signal/outdir_hadronic_0p_BDT3binTuneV$i/CMS-HGG_mva_13TeV_sigfit.root CMS-HGG_mva_13TeV_0p_hadronic_sigfit_v"$i"_bdt.root  
 	ln -s /afs/cern.ch/work/x/xiaomeng/test/myWorkingArea/CMSSW_8_1_0/src/flashggFinalFit/Signal/outdir_lepronic_0m_BDT3binTuneV$i/CMS-HGG_mva_13TeV_sigfit.root CMS-HGG_mva_13TeV_0m_leptonic_sigfit_v"$i"_bdt.root  
 	ln -s /afs/cern.ch/work/x/xiaomeng/test/myWorkingArea/CMSSW_8_1_0/src/flashggFinalFit/Signal/outdir_hadronic_0m_BDT3binTuneV$i/CMS-HGG_mva_13TeV_sigfit.root CMS-HGG_mva_13TeV_0m_hadronic_sigfit_v"$i"_bdt.root  
-	cd $cur
 	for j in leptonic hadronic
 	do
 		for k in {1..2}
 		do
+	cd $cur
 			cp CMS-HGG_mva_13TeV_datacard_TEMPLATE_percat.txt optimization/CMS-HGG_mva_13TeV_datacard_"$j"_cat"$k"_v"$i"_bdt.txt 
 			filename=optimization/CMS-HGG_mva_13TeV_datacard_"$j"_cat"$k"_v"$i"_bdt.txt
 			t1=$(((k-1)*3))
@@ -36,4 +36,5 @@ do
 			text2workspace.py CMS-HGG_mva_13TeV_datacard_"$j"_cat"$k"_v"$i"_bdt.txt -o CMS-HGG_mva_13TeV_"$j"_cat"$k"_v"$i"_bdt.root -P HiggsAnalysis.CombinedLimit.HiggsJPC:twoHypothesisHiggs  --PO=muFloating
 			combine CMS-HGG_mva_13TeV_"$j"_cat"$k"_v"$i"_bdt.root -M MultiDimFit --algo=grid  --points=50  -n "$j"_cat"$k"_v"$i"_bdt --setParameterRanges r=0.00,4.0 -m 125.00 -t -1  --expectSignal 1.00 -S 0 --freezeParameters MH 
 	done
+done
 done
